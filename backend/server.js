33c20ee10cf2e9a6c7f36ac78770430d1be73663
Bhavna-07 <olivplath@gmail.com>
2025-02-07 20:57:20 +0530

    app running at same port

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import userRoutes from './routes/user.route.js'
 import postRoutes from './routes/post.route.js'
 import notiticationRoutes from './routes/notification.route.js'
 import {v2 as cloudinary} from 'cloudinary'
+import path from 'path'
 
 dotenv.config()
 cloudinary.config({
@@ -18,6 +19,7 @@ cloudinary.config({
 
 const app = express()
 const port = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(express.json({limit: "5mb"}))
 app.use(cors())
@@ -28,6 +30,13 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notiticationRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 app.listen(port, () => {
   console.log(`App is Listening at http://localhost:${port}`)
